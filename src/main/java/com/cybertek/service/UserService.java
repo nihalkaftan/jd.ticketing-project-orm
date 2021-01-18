1048689d63f9ac1c689b8895c709d482d534805d
nihalkaftan <nihalkaftan@gmail.com>
2021-01-17 19:18:31 -0500

    Ticketing Project ORM

---
+++ src/main/java/com/cybertek/service/UserService.java
@@ -0,0 +1,19 @@
+package com.cybertek.service;
+
+import com.cybertek.dto.UserDTO;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<UserDTO> listAllUsers();
+
+    UserDTO findByUserName(String userName);
+
+    void save(UserDTO dto);
+
+    UserDTO update(UserDTO dto);
+
+    void delete(String username);
+
+}
