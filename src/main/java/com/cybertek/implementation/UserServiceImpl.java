ac899c072299fdac79e641c06fadfdb937122579
nihalkaftan <nihalkaftan@gmail.com>
2021-01-18 16:29:12 -0500

    Ticketing Project ORM

---
+++ src/main/java/com/cybertek/implementation/UserServiceImpl.java
@@ -0,0 +1,67 @@
+package com.cybertek.implementation;
+
+import com.cybertek.dto.UserDTO;
+import com.cybertek.entity.User;
+import com.cybertek.mapper.UserMapper;
+import com.cybertek.repository.UserRepository;
+import com.cybertek.service.UserService;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    UserRepository userRepository;
+    UserMapper userMapper;
+
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    public List<UserDTO> listAllUsers() {
+        List<User> list = userRepository.findAll(Sort.by("firstName"));
+        return list.stream().map(obj -> {return userMapper.convertToDto(obj);}).collect(Collectors.toList());
+    }
+
+    @Override
+    public UserDTO findByUserName(String username) {
+        User user = userRepository.findByUserName(username);
+
+        return userMapper.convertToDto(user);
+    }
+
+    @Override
+    public void save(UserDTO dto) {
+        User obj = userMapper.convertToEntity(dto);
+        userRepository.save(obj);
+    }
+
+    @Override
+    public UserDTO update(UserDTO dto) {
+        //find current user
+        User user= userRepository.findByUserName(dto.getUserName());
+        //Map update user to dto object
+        User convertedUser = userMapper.convertToEntity(dto);
+        //set id to the converted object
+        convertedUser.setId(user.getId());
+        //save updated user
+        userRepository.save(convertedUser);
+
+        return findByUserName(dto.getUserName());
+    }
+
+    @Override
+    public void delete(String username) {
+
+    }
+
+    @Override
+    public void deleteByUserName(String username) {
+        userRepository.deleteByUserName(username);
+    }
+}
--- src/main/java/com/cybertek/service/UserService.java
+++ src/main/java/com/cybertek/service/UserService.java
@@ -8,12 +8,14 @@ public interface UserService {
 
     List<UserDTO> listAllUsers();
 
-    UserDTO findByUserName(String userName);
+    UserDTO findByUserName(String username);
 
     void save(UserDTO dto);
 
     UserDTO update(UserDTO dto);
 
     void delete(String username);
+
+    void deleteByUserName(String username);
 
 }
